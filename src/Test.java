f05089dc35c29669b5ba8d4b9cef0d5037370d4b
whymeeverytime <borgahaktanbilen01@gmail.com>
2020-12-17 14:21:02 +0300

    init

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+import functionlibrary.*;
+
+/**
+ * Tester for function library 
+ * @author Borga Haktan Bilen
+*/ 
+public class Test {
+    public static void main( String[] args ) {
+        System.out.printf( "%n%s%d%n", "The factorial of 5 (5!): ", Func.factorial( 5 ) );
+    }
+}
---
+++ src/functionlibrary/Func.java
@@ -0,0 +1,26 @@
+package functionlibrary;
+
+public class Func {
+
+    public static long factorial( int theNumber ) {
+        if ( theNumber == 1 ) {
+            return 1;
+        }
+        else {
+            return theNumber * factorial( theNumber - 1 );
+        }
+    }
+    /* This method is for entering something bigger than integer limit.
+     * However, result will still overflow if I enter smt. bigger than int. limit.
+     * Sooooo, work in progress :/
+    */
+    public static long factorial( long theNumber ) {
+
+        if ( theNumber == 1 ) {
+            return 1;
+        }
+        else {
+            return theNumber * factorial( theNumber - 1 );
+        }
+    }
+}
